95c636791d596d770f9da438062508afd5b85bc9
Hridoy Sheikh <ridoysheikh15@gmail.com>
2024-08-18 02:26:21 +0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,3 +6,13 @@ function nav_btn() {
         nav_item.style.display = "block";
     }
 }
+
+function my_age() {
+    let res = document.getElementById('my_age');
+    let birthDate = new Date(2003, 4, 31); // Month is 0-indexed, so May is 4
+    let currentDate = new Date();
+    let timeDifference = currentDate - birthDate;
+    let ageDate = new Date(timeDifference); 
+    let age = Math.abs(ageDate.getUTCFullYear() - 1970);
+    res.innerHTML = age;
+}
